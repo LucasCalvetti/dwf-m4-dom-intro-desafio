147263d01de3aefd22f76c4782dacdcceab4f677
Lucas Calvetti <lic.calvo099@gmail.com>
2021-06-08 01:58:59 -0300

    desafio terminado

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 const cosasQueAprendimos = [
-  {
-    tema: "terminal",
-    class: "",
-  },
-  {
-    tema: "node",
-    class: "",
-  },
-  {
-    tema: "oop",
-    class: "",
-  },
-  {
-    tema: "typescript",
-    class: "",
-  },
-  {
-    tema: "css",
-    class: "",
-  },
-  {
-    tema: "dom",
-    class: "special",
-  },
+    {
+        tema: "terminal",
+        class: "",
+    },
+    {
+        tema: "node",
+        class: "",
+    },
+    {
+        tema: "oop",
+        class: "",
+    },
+    {
+        tema: "typescript",
+        class: "",
+    },
+    {
+        tema: "css",
+        class: "",
+    },
+    {
+        tema: "dom",
+        class: "special",
+    },
 ];
 
-function main() {}
+function main() {
+    const liEls = document.querySelectorAll(".lista li");
+    for (let el of liEls) {
+        el.remove();
+    }
+    const ul = document.querySelector(".lista");
+    cosasQueAprendimos.forEach((item) => {
+        let newLi = document.createElement("li");
+        newLi.textContent = item.tema;
+        newLi.className = item.class;
+        ul.appendChild(newLi);
+    });
+}
 
 main();
